5af6e4c6d30b1ffd8e3c7eecae011ad7f540c3fe
HUANG Cheng <cheng@duck.com>
2024-01-04 22:23:34 +0800

    add the emojis data

--- emojidataprovider.cpp
+++ emojidataprovider.cpp
@@ -1,11 +1,21 @@
 #include "emojidataprovider.h"
 
+#include <QFile>
+
 EmojiDataProvider::EmojiDataProvider(QObject *parent)
     : QObject{parent}
 {
-    QJsonDocument doc = QJsonDocument::fromJson(emojis);
 
-    m_document = Document().read(doc.object()).get();
+    QFile file(":/cheng.im/EmojiPicker/data/emojis.json");
+
+    if (!file.open(QIODevice::ReadOnly))
+    {
+        qErrnoWarning("Unable to read");
+    } else {
+        QJsonDocument doc = QJsonDocument::fromJson(file.readAll());
+
+        m_document = Document().read(doc.object()).get();
+    }
 }
 
 QStringList EmojiDataProvider::categories() {
